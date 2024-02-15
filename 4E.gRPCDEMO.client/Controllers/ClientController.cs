f712f3c8aa27de2e24e12c228cfb2bce3cdf8c0c
nada <nadaelneel29@gmail.com>
2024-02-15 12:38:26 +0200

    client

---
+++ 4E.gRPCDEMO.client/Controllers/ClientController.cs
@@ -0,0 +1,24 @@
+﻿using _4E.gRPCDemo.Server.Protos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _4E.gRPCDEMO.client.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        private readonly TrackingServiceClientWrapper _Client;
+
+        public ClientController(TrackingServiceClientWrapper Client)
+        {
+            _Client = Client;
+        }
+        [HttpPost]
+        public async Task<IActionResult> YourActionMethod( TrackingMessage request)
+        {
+            var response = await _Client.SendRequest(request);
+            return Ok(response);
+        }
+    }
+}
---
+++ 4E.gRPCDEMO.client/TrackingServiceClientWrapper.cs
@@ -0,0 +1,24 @@
+﻿using _4E.gRPCDemo.Server.Protos;
+using Grpc.Core;
+using Grpc.Net.Client;
+using System.Threading.Channels;
+
+namespace _4E.gRPCDEMO.client
+{
+    public class TrackingServiceClientWrapper
+    {
+        private readonly TrackingService.TrackingServiceClient _client;
+
+        public TrackingServiceClientWrapper()
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:7271/"); ;
+            _client = new TrackingService.TrackingServiceClient(channel);
+        }
+
+        public async Task<TrackingResponse> SendRequest(TrackingMessage request)
+        {
+            var response = await _client.SendMessageAsync(request);
+            return response;
+        }
+    }
+}
